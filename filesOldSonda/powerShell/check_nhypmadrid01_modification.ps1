1af3a072aa2b173b547f66819a8ff229051588f4
Javier Bello <javier.bello@dxc.com>
2024-11-20 12:54:31 +0000

    new file:   filesNewSonda/jsonScripts/ebsa.json new file:   filesNewSonda/jsonScripts/test.json new file:   filesNewSonda/telegraf.d/PA_nhypmadrid01.conf.disabled new file:   filesNewSonda/telegraf.d/WinServices.conf new file:   filesNewSonda/telegraf.d/ebsa.conf new file:   filesNewSonda/telegraf.d/hyperion.conf new file:   filesNewSonda/telegraf.d/hyperion.conf.ok new file:   filesNewSonda/telegraf.d/hyperion.conf.old new file:   filesNewSonda/telegraf.d/hyperion_poc.conf new file:   filesNewSonda/telegraf.d/mtool.conf new file:   filesNewSonda/telegraf.d/output.conf new file:   filesNewSonda/telegraf.d/windows.conf new file:   filesOldSonda/jsonScripts/PowerAutomate_Aplicacion_M3.json new file:   filesOldSonda/jsonScripts/PowerAutomate_ebsapl.json new file:   filesOldSonda/jsonScripts/PowerAutomate_nhypmadrid01.json new file:   filesOldSonda/powerShell/check_M3_modification.ps1 new file:   filesOldSonda/powerShell/check_ebsapl_modification.ps1 new file:   filesOldSonda/powerShell/check_nhypmadrid01_modification.ps1 new file:   filesOldSonda/powerShell/restart_rpa_agent.ps1 new file:   filesOldSonda/telegraf.d/PA_M3.conf new file:   filesOldSonda/telegraf.d/PA_M3.conf.disabled new file:   filesOldSonda/telegraf.d/PA_ebsapl.conf new file:   filesOldSonda/telegraf.d/PA_ebsapl.conf.disabled new file:   filesOldSonda/telegraf.d/PA_nhypmadrid01.conf new file:   filesOldSonda/telegraf.d/PA_nhypmadrid01.conf.disabled new file:   filesOldSonda/telegraf.d/WinServices.conf new file:   filesOldSonda/telegraf.d/output.conf new file:   filesOldSonda/telegraf.d/windows.conf

---
+++ filesOldSonda/powerShell/check_nhypmadrid01_modification.ps1
@@ -0,0 +1,21 @@
+# Ruta del archivo
+$filePath = "C:\M360\Power Automate\PowerAutomate_nhypmadrid01.json"
+
+# Obtener la fecha de modificación del archivo
+$fileInfo = Get-Item $filePath
+$lastModified = $fileInfo.LastWriteTime
+
+# Obtener la fecha y hora actual
+$currentTime = Get-Date
+
+# Calcular la diferencia en minutos
+$timeDifference = ($currentTime - $lastModified).TotalMinutes
+
+# Verificar si la diferencia es menor o igual a 20 minutos
+if ($timeDifference -le 20) {
+    # Leer y imprimir el contenido del archivo
+    Get-Content $filePath
+} else {
+    # Imprimir un mensaje de que el archivo no ha sido modificado recientemente
+    Write-Output "{}"  # Devuelve un JSON vacío para que Telegraf no falle
+}
